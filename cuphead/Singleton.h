9ede96aefdcfc83df39c08302319ff7aaaaba2a1
orange2458 <asm1133@naver.com>
2024-02-07 22:28:45 +0900

    Add singleton macro

---
+++ cuphead/Singleton.h
@@ -0,0 +1,14 @@
+#ifndef __Singleton
+#define __Singleton
+
+#define SINGLETON( type ) public:\
+							  static type* GetInst( )\
+							  {\
+								  static type mgr;\
+								  return &mgr;\
+							  }\
+						  private:\
+							  type( );\
+							  ~type( );
+
+#endif	// __Singleton
