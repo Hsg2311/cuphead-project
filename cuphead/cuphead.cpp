c6959f941ebe5c01689ddcd9c527fccc8bb19d7c
orange2458 <asm1133@naver.com>
2024-02-08 02:43:01 +0900

    Remove menu

--- cuphead/cuphead.cpp
+++ cuphead/cuphead.cpp
@@ -86,7 +86,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CUPHEAD));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CUPHEAD);
+    wcex.lpszMenuName   = nullptr;
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
