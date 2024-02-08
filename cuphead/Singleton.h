865188082398921e6ecc1c2197c18444741aaf43
orange2458 <asm1133@naver.com>
2024-02-08 21:04:43 +0900

    Adjust indentation coherently

--- cuphead/Singleton.h
+++ cuphead/Singleton.h
@@ -1,14 +1,14 @@
 #ifndef __Singleton
 #define __Singleton
 
-#define SINGLETON( type ) public:\
-							  static type* GetInst( )\
-							  {\
-								  static type mgr;\
-								  return &mgr;\
-							  }\
-						  private:\
-							  type( );\
-							  ~type( );
+#define SINGLETON(type) public:\
+							static type* GetInst( )\
+							{\
+								static type mgr;\
+								return &mgr;\
+							}\
+						private:\
+							type( );\
+							~type( );
 
 #endif	// __Singleton
